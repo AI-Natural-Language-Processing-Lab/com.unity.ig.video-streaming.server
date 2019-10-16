dd65c9280aac831b9bd4e1ddc206b423e3bc515b
Etienne Cella <etiennec@hq.unity3d.com>
2019-10-16 15:19:54 -0400

    1st commit, namespace, folder structure, not quite a package yet, well get there

---
+++ UnityExamples~/EncodeToRTSP/Assets/Scripts/TimeUpdate.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeUpdate : MonoBehaviour
+{
+    public int frameRate = 30;
+    
+    void Update()
+    {
+        var t = Time.unscaledTime;
+        var seconds = (int)t;
+        var h = seconds / 3600;
+        var hInSec = h * 3600;
+        var m = (seconds - hInSec) / 60;
+        var mInSec = m * 60;
+        var s = (seconds - (hInSec + mInSec));
+        var framesDouble = frameRate * (t - (double) seconds);
+        var f = (int)framesDouble;
+        //var frac = 100 * (framesDouble - f) / frameRate;
+        GetComponent<Text>().text = $"{h:00}:{m:00}:{s:00}:{f:00}";
+    }
+}
